e8d41c19324fc85e0e90acf8a5c405907a2eb05b
ab.en3rgy <ab.en3rgy@gmail.com>
2021-04-21 22:53:35 +0400

    fix some style errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import greet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = greet();
-console.log('Hi ' + name); 
+console.log(`Hi ${name}`);
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
- 
+
 const greet = () => readlineSync.question('May I have your name? ');
 export default greet;
