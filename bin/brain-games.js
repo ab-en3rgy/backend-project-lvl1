e956a1ca0cab2a6d4ef274dc07c739a707689d78
nrgy <ab.en3rgy@gmail.com>
2022-05-26 20:53:35 +0300

    another commit

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,4 +4,4 @@ import question from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const name = question('May I have your name?');
-console.log(`Hi ${name}`);
+console.log(`Hello, ${name}`);
